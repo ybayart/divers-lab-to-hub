a889d9b6b1ffd56f2b0d8dee4edeac7a195dd6dd
YanYan <yanyan@42paris.fr>
2020-11-03 15:00:59 +0100

    add line count

---
+++ count_lines.sh
@@ -0,0 +1,13 @@
+DIR=repo/
+rm -f out
+find $DIR -name '*.c' -exec cat {} \;>>out
+find $DIR -name '*.h' -exec cat {} \;>>out
+find $DIR -name '*.cpp' -exec cat {} \;>>out
+find $DIR -name '*.hpp' -exec cat {} \;>>out
+find $DIR -name '*.s' -exec cat {} \;>>out
+find $DIR -name '*.yaml' -exec cat {} \;>>out
+find $DIR -name '*.rb' -exec cat {} \;>>out
+find $DIR -name 'Dockerfile' -exec cat {} \;>>out
+find $DIR -name 'docker-compose.yml' -exec cat {} \;>>out
+wc out
+rm -f out
