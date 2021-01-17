6046ab0d626da0d26e4dc9afad2c4fc8de94844d
YanYan <yanyan@hexanyn.fr>
2021-01-17 07:24:11 +0100

    add .py & .sh files

--- count_lines.sh
+++ count_lines.sh
@@ -7,6 +7,8 @@ find $DIR -name '*.hpp' -exec cat {} \;>>out
 find $DIR -name '*.s' -exec cat {} \;>>out
 find $DIR -name '*.yaml' -exec cat {} \;>>out
 find $DIR -name '*.rb' -exec cat {} \;>>out
+find $DIR -name '*.py' -exec cat {} \;>>out
+find $DIR -name '*.sh' -exec cat {} \;>>out
 find $DIR -name 'Dockerfile' -exec cat {} \;>>out
 find $DIR -name 'docker-compose.yml' -exec cat {} \;>>out
 wc out
